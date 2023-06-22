ddaa317cefe30220b9bdd393266162483f6b5f17
Antoine <ant.griffit@gmail.com>
2023-06-22 11:53:35 +0200

    token

---
+++ src/Controller/BookController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use Doctrine\ORM\EntityManager;
+use App\Repository\BookRepository;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'book', methods:['GET'])]
+    public function getBookList(BookRepository $bookRepository,SerializerInterface $serializer): JsonResponse
+    {
+        $bookList = $bookRepository->findAll();
+        $jsonBookList = $serializer->serialize($bookList,'json',['groups'=>'getBooks']);
+
+        return new JsonResponse($jsonBookList,Response::HTTP_OK,[],true);
+    }
+
+    #[Route('/api/book/{id}',name:"detailBook",methods:['GET'])]
+    public function getDetailBook(Book $book,SerializerInterface $serializer):JsonResponse{
+
+        $jsonBook = $serializer->serialize($book,'json',['groups'=>'getBooks']);
+        return new JsonResponse($jsonBook, Response::HTTP_OK,['accept'=>'json'],true);
+    }
+
+    #[Route('/api/book/{id}',name:'deleteBook',methods:['DELETE'])]
+    public function deleteBook(Book $book, EntityManagerInterface $manager):JsonResponse{
+
+        $manager->remove($book);
+        $manager->flush();
+
+        return new JsonResponse(null,Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/books',name:"createBook",methods:['POST'])]
+    public function createBook(
+        SerializerInterface $serializer,
+        Request $request,
+        EntityManagerInterface $manager,
+        UrlGeneratorInterface $urlGenerator, 
+        AuthorRepository $authorRepository,
+        ValidatorInterface $validator):JsonResponse{
+
+        $book = $serializer->deserialize($request->getContent(),Book::class,'json');
+
+        //On vérifie les erreurs
+        $errors = $validator->validate($book);
+        if($errors->count()>0){
+            return new JsonResponse($serializer->serialize($errors,'json'),JsonResponse::HTTP_BAD_REQUEST,[],true);
+        }
+
+        // Récupération de l'ensemble des données envoyées sousforme de tableau
+        $content = $request->toArray();
+
+        $idAuthor=$content['idAuthor']??-1;
+        // On cherche l'auteur qui correspond et on l'assigne au livre.
+        // Si "find" ne trouve pas l'auteur, alors null sera retourné.
+
+        $book->setAuthor($authorRepository->find($idAuthor));
+
+        $manager->persist($book);
+        $manager->flush();
+
+        $jsonBook = $serializer->serialize($book,'json',['groups'=>"getBooks"]);
+
+        $location = $urlGenerator->generate('detailBook',['id'=>$book->getId()],UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonBook,Response::HTTP_CREATED,["location"=>$location],true);
+    }
+
+    #[Route('/api/Books/{id}',name:'updateBook',methods:['PUT'])]
+    public function updateBook(SerializerInterface $serializer,Request $request,Book $currentBook,AuthorRepository $authorRepository,EntityManagerInterface $manager):JsonResponse{
+
+        $updateBook = $serializer->deserialize($request->getContent(),Book::class,'json',[AbstractNormalizer::OBJECT_TO_POPULATE => $currentBook]);
+        $content = $request->toArray();
+        $idAuthor = $content['idAuthor']??-1;
+        $updateBook->setAuthor($authorRepository->find($idAuthor));
+
+        $manager->persist($updateBook);
+        $manager->flush();
+
+       return new JsonResponse(null,JsonResponse::HTTP_NO_CONTENT);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Book;
+use App\Entity\User;
+use App\Entity\Author;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        //Création d'un user "normal"
+        $user = new User;
+        $user->setEmail("user@bookapi.com");
+        $user->setRoles(["Role_USER"]);
+
+        $user->setPassword($this->userPasswordHasher->hashPassword($user,"password"));
+
+        $manager->persist($user);
+
+        //Création d'un user Admin
+        $userAdmin = new User;
+        $userAdmin->setEmail("admin@bookapi.com");
+        $userAdmin->setRoles(["Role_ADMIN"]);
+
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin,"password"));
+
+        $manager->persist($userAdmin);
+
+        //Création d'une dizaine d'auteurs
+        $listAuthor = [];
+        for ($i = 0; $i < 10; $i++) {
+        // Création de l'auteur lui-même.
+        $author = new Author();
+        $author->setFirstName("Prénom " . $i);
+        $author->setLastName("Nom " . $i);
+        $manager->persist($author);
+        // On sauvegarde l'auteur créé dans un tableau.
+        $listAuthor[] = $author;
+        }
+
+        // Création d'une vingtaine de livres ayant pour titre
+        for ($i = 0; $i < 20; $i++) {   
+            $livre = new Book;
+            $livre->setTitle('Livre ' . $i);
+            $livre->setCoverText('Quatrième de couverture numéro :'. $i);
+
+            // On lie le livre à un auteur pris au hasard dans letableau des auteurs.
+            $livre->setAuthor($listAuthor[array_rand($listAuthor)]);
+            $manager->persist($livre);
+            }
+    $manager->flush();
+    
+    }
+}
